7afd3c56d16b5d92dbf3e57127b62b56c0dce0f1
bxpluse <21994296+bxpluse@users.noreply.github.com>
2021-01-10 22:17:20 -0500

    Enchance config tables

--- luminaria-server/database/local_config_model.py
+++ luminaria-server/database/local_config_model.py
@@ -6,6 +6,8 @@ from database.base_model import BaseModel
 class LocalConfigModel(BaseModel):
     parameter = CharField(unique=True)
     value = CharField()
+    description = CharField(null=True)
+    name = CharField(null=True)
 
     class Meta:
         table_name = 'LOCAL_CONFIG'
